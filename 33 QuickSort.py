f9994ee3aee7b24bed9dd52cfee65bba1593759f
hugoallan9 <hugoallangm@gmail.com>
2021-10-12 07:48:49 -0600

    Intento de usar como pivote el punto medio

--- 33 QuickSort.py
+++ 33 QuickSort.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+from math import floor
 
 numeros = np.random.randn(1,int(1e1)).tolist()[0]
 
@@ -10,17 +11,17 @@ def quicksort(start, end, array):
         quicksort(p+1,end,array)
 
 def partition(start,end,array):
-    pivot_index = start
+    pivot_index = floor( (start + end) / 2 )
     pivot = array[pivot_index]
 
     while start < end:
-        while start < len(array) and array[start] <= pivot:
+        while  array[start] <= pivot:
             start +=1
 
         while array[end] > pivot:
             end -= 1
 
-        if (start < end):
+        if (start <= end):
             array[start], array[end] = array[end], array[start]
 
     array[end], array[pivot_index] = array[pivot_index], array[end]
@@ -32,7 +33,7 @@ inicio = time.time()
 quicksort(0,len(numeros)-1,numeros)
 fin = time.time()
 print(' \n Tiempo de ejecución:', fin-inicio)
-
+print(numeros)
 
 
 
